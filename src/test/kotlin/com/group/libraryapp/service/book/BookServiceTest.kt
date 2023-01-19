9071567d670ed592463165ad6fe4b6e767a25979
prolkh <prolkh@gmail.com>
2023-01-19 22:54:28 +0900

    서비스 계층을 Kotlin 으로 변경하고 Optional 을 제거하다.

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -78,7 +78,7 @@ class BookServiceTest @Autowired constructor(
         val message = assertThrows<IllegalArgumentException> {
             bookService.loanBook(request)
         }.message
-        assertThat(message).isEqualTo("진작 대출되어 있는 책입니다")
+        assertThat(message).isEqualTo("진작 대출되어 있는 책입니다.")
     }
 
     @Test
