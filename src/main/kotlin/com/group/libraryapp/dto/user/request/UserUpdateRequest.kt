d8fd04b960aefd1b0960972ab33907e03098696a
prolkh <prolkh@gmail.com>
2023-01-20 23:44:18 +0900

    DTO 계층을 Kotlin 으로 변경하다.

--- src/main/kotlin/com/group/libraryapp/dto/user/request/UserUpdateRequest.kt
+++ src/main/kotlin/com/group/libraryapp/dto/user/request/UserUpdateRequest.kt
@@ -1,21 +1,6 @@
-package com.group.libraryapp.dto.user.request;
+package com.group.libraryapp.dto.user.request
 
-public class UserUpdateRequest {
-
-  public UserUpdateRequest(long id, String name) {
-    this.id = id;
-    this.name = name;
-  }
-
-  private long id;
-  private String name;
-
-  public long getId() {
-    return id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-}
+data class UserUpdateRequest(
+    val id: Long,
+    val name: String
+)
