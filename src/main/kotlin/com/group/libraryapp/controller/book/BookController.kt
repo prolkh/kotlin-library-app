7eb5d822876e83b666415b1e84d165b438ce5dd4
prolkh <prolkh@gmail.com>
2023-01-30 23:57:09 +0900

    타입별 책 통계 조회 함수를 작성하다.

--- src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
+++ src/main/kotlin/com/group/libraryapp/controller/book/BookController.kt
@@ -3,6 +3,7 @@ package com.group.libraryapp.controller.book
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.service.book.BookService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -32,5 +33,10 @@ class BookController(
     @GetMapping("book/loan")
     fun countLoanedBook(): Int{
         return bookService.countLoanBook()
+    }
+
+    @GetMapping("/book/stat")
+    fun getBookStatistics(): List<BookStatResponse> {
+        return bookService.getBookStatistics()
     }
 }
