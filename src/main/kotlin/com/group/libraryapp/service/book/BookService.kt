42ae4e137fb55183c7214679e5713f0ba296aa78
prolkh <prolkh@gmail.com>
2023-01-31 23:58:21 +0900

    groupBy를 사용하여 통계 함수를 리팩토링하다.

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -50,13 +50,8 @@ class BookService (
 
     @Transactional(readOnly = true)
     fun getBookStatistics(): List<BookStatResponse> {
-        val result = mutableListOf<BookStatResponse>()
-        val books = bookRepository.findAll()
-
-        for (book in books) {
-            result.firstOrNull { dto -> book.type == dto.type }?.plusOne()
-                ?: result.add(BookStatResponse(book.type, 1))
-        }
-        return result
+        return bookRepository.findAll()     // List<Book>
+            .groupBy { book -> book.type }  // Map<BookType, List<Book>>
+            .map { (type, books) -> BookStatResponse(type, books.size) }    // List<BookStatResponse>
     }
 }
